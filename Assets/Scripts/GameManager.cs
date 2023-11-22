4dd4e4f4b635e532ef5fbf9b031cf4859480acfd
Jaufret <jaufret.dufeil@etudiant.univ-rennes1.fr>
2023-11-22 17:34:28 +0100

    debut rollABoll++, ajout 2eme zone + portes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour{
+
+    private int count;
+    private float currentTime; // in seconds
+
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI currentTimeText;
+    public GameObject winTextObject;
+
+    public static GameManager Manager;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetCountText();
+        winTextObject.SetActive(false);
+        currentTime = 0;
+        SetCurrentTimeText();
     }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void SetCurrentTimeText()
+    {
+        currentTimeText.text = "Chrono: " + currentTime.ToString("00.0");
+    }
+
+    public void Awake()
+    {
+        if(GameManager.Manager != null) { Destroy(this); }
+        Manager = this;
+    }
+
+
+     public void Collect(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            count = count + 1;
+            other.gameObject.SetActive(false);
+            SetCountText();
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+        SetCurrentTimeText();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,57 +6,40 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour{
 
-    private int count;
-
     private float movementX;
     private float movementY;
 
     private Rigidbody rb;
 
     public float speed = 0;
-    public TextMeshProUGUI countText;
-    public GameObject winTextObject;
+
+    public GameManager Manager;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        count = 0;
         rb = GetComponent<Rigidbody>();
-        SetCountText();
-        winTextObject.SetActive(false);
-
     }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
-
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            Manager.Collect(other);
+        }
+    }
 
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
-    }
-
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 10)
-        {
-            winTextObject.SetActive(true);
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            count = count + 1;
-            other.gameObject.SetActive(false);
-            SetCountText();
-        }
     }
 
     // Update is called once per frame
